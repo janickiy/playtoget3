cd8e28ea4a72cff1f5cd366e3c0e72d657de98e5
janickkiy <janickiy@mail.ru>
2026-06-20 19:41:35 +0300

    bugfix

--- database/seeders/data/settings.php
+++ database/seeders/data/settings.php
@@ -4,6 +4,26 @@ $now = now();
 
 return [
     [
+        'key_cd' => 'EMAIL',
+        'name' => 'info@playtoget.com',
+        'type' => 'TEXT',
+        'display_value' => 'Email',
+        'value' => 'info@playtoget.com',
+        'published' => 1,
+        'created_at' => $now,
+        'updated_at' => $now,
+    ],
+    [
+        'key_cd' => 'NAME',
+        'name' => 'Project name',
+        'type' => 'TEXT',
+        'display_value' => 'Project name',
+        'value' => 'PlayToGet',
+        'published' => 1,
+        'created_at' => $now,
+        'updated_at' => $now,
+    ],
+    [
         'key_cd' => 'SLOGAN',
         'name' => 'Site slogan',
         'type' => 'TEXT',
