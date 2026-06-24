c753a67351b4cae6f09aba732231ede9377499ed
janickkiy <janickiy@mail.ru>
2026-06-24 21:09:33 +0300

    bugfix

--- resources/views/front/profile/edit.blade.php
+++ resources/views/front/profile/edit.blade.php
@@ -45,7 +45,7 @@
 @push('styles')
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery-ui-1.8.16.custom.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery.Jcrop.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/profile-settings.css') }}?v=2026062401">
+    <link rel="stylesheet" href="{{ asset('frontend/css/profile-settings.css') }}?v=2026062402">
 @endpush
 
 @section('content')
@@ -380,5 +380,5 @@
 @push('scripts')
     <script src="{{ asset('frontend/js/jquery-ui.min.js') }}"></script>
     <script src="{{ asset('frontend/js/jquery.Jcrop.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/profile-settings.js') }}?v=2026062004"></script>
+    <script src="{{ asset('frontend/js/profile-settings.js') }}?v=2026062402"></script>
 @endpush
